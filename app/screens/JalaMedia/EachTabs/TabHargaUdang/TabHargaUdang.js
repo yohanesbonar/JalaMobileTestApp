18890312f109739f493bc878e7fff7ee53f312d4
yohanesbonar <yohanesbonar@gmail.com>
2022-05-23 00:26:25 +0700

    create pagination harga udang

--- app/screens/JalaMedia/EachTabs/TabHargaUdang/TabHargaUdang.js
+++ app/screens/JalaMedia/EachTabs/TabHargaUdang/TabHargaUdang.js
@@ -1,27 +1,48 @@
-import {View, Toast} from 'native-base';
+import {View, Toast, Icon, Spinner} from 'native-base';
 import React, {Fragment, useEffect, useState} from 'react';
 import {FlatList, Text, StyleSheet} from 'react-native';
 import CardItemHargaUdang from '../../../../components/molecules/CardItemHargaUdang';
 import {getHargaUdang} from '../../../../utils/network/HargaUdang';
+import _ from 'lodash';
 
 const TabHargaUdang = ({navigation}) => {
   const [page, setPage] = useState(1);
   const [data, setData] = useState([]);
+  const [isEmptyData, setIsEmptyData] = useState(false);
+  const [isRefresh, setIsRefresh] = useState(false);
+  const [isFetching, setIsFetching] = useState(false);
+  const [limit, setLimit] = useState(15);
+  const [isFailed, setIsFailed] = useState(false);
 
   useEffect(() => {
     getData();
   }, []);
 
   const getData = async () => {
-    let limit = 15;
-
+    setIsFetching(true);
+    setIsRefresh(false);
+    setIsFailed(false);
     try {
       let response = await getHargaUdang(limit, page);
       console.log('response', response);
-      if (response.data) {
-        setData(response.data);
+      let result = response.data;
+      if (response) {
+        setIsEmptyData(_.isEmpty(result) ? true : false);
+        setData(page == 1 ? result : [...data, ...result]);
+        setPage(page + 1);
+        setIsFailed(false);
+        setIsFetching(false);
+      } else {
+        setIsFailed(true);
+        setIsFetching(false);
+        Toast.show({
+          title: 'Something went wrong!!',
+          duration: 1500,
+        });
       }
     } catch (error) {
+      setIsFailed(true);
+      setIsFetching(false);
       Toast.show({
         title: 'Something went wrong!!' + error,
         duration: 1500,
@@ -56,7 +77,71 @@ const TabHargaUdang = ({navigation}) => {
     );
   };
 
-  return <FlatList data={data} renderItem={_renderItem} />;
+  const handleLoadMore = () => {
+    if (isEmptyData == true || isFetching == true) {
+      return;
+    }
+    getData();
+  };
+
+  const _renderItemFooter = () => (
+    <View
+      style={[
+        styles.containerItemFooter(isFetching),
+        {width: isFetching == true ? '100%' : null},
+      ]}>
+      {_renderItemFooterLoader()}
+    </View>
+  );
+
+  const _renderItemFooterLoader = () => {
+    if (isFailed == true && page > 1) {
+      return (
+        <TouchableOpacity
+          onPress={() => {
+            handleLoadMore();
+          }}>
+          <Icon name="ios-sync" style={{fontSize: 42}} />
+        </TouchableOpacity>
+      );
+    }
+
+    if (isFetching == true) {
+      return (
+        <View style={{marginVertical: 16}}>
+          <Spinner color="indigo.500" size={'lg'} />
+        </View>
+      );
+    }
+  };
+
+  const onRefreshData = () => {
+    setData([]);
+    setPage(1);
+    setIsFetching(true);
+    setIsEmptyData(false);
+    setIsRefresh(true);
+  }
+
+  useEffect(() => {
+    if (isRefresh == true) {
+      setIsRefresh(false);
+      getData();
+    }
+  }, [isRefresh == true]);
+
+  return (
+    <FlatList
+      data={data}
+      renderItem={_renderItem}
+      keyExtractor={(item, index) => index.toString()}
+      onEndReachedThreshold={0.5}
+      onEndReached={handleLoadMore}
+      ListFooterComponent={_renderItemFooter()}
+      onRefresh={() => onRefreshData()}
+      refreshing={false}
+    />
+  );
 };
 export default TabHargaUdang;
 
@@ -69,4 +154,10 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     marginBottom: -8,
   },
+  containerItemFooter: isFetching => [
+    {
+      justifyContent: isFetching ? null : 'center',
+      alignItems: isFetching ? null : 'center',
+    },
+  ],
 });
