f8bda10306ce47604c01406364fad36efeb31166
yohanesbonar <yohanesbonar@gmail.com>
2022-05-23 00:03:33 +0700

    create layout harga terbaru

--- app/screens/JalaMedia/EachTabs/TabHargaUdang/TabHargaUdang.js
+++ app/screens/JalaMedia/EachTabs/TabHargaUdang/TabHargaUdang.js
@@ -1,6 +1,6 @@
 import {View, Toast} from 'native-base';
 import React, {Fragment, useEffect, useState} from 'react';
-import {FlatList, Text} from 'react-native';
+import {FlatList, Text, StyleSheet} from 'react-native';
 import CardItemHargaUdang from '../../../../components/molecules/CardItemHargaUdang';
 import {getHargaUdang} from '../../../../utils/network/HargaUdang';
 
@@ -35,22 +35,38 @@ const TabHargaUdang = ({navigation}) => {
   };
   const _renderItem = ({item, index}) => {
     return (
-      <CardItemHargaUdang
-        data={item}
-        supplierName={
-          item.creator.email_verified ? item.creator.name : item.creator.buyer
-        }
-        verified={item.creator.email_verified ? true : false}
-        date={item.updated_at}
-        avatarId={item.creator.avatar}
-        regencyName={item.region.regency_name}
-        provinceName={item.region.province_name}
-        size={50}
-        onPressDetail={() => goToDetail(item)}
-      />
+      <View>
+        {index == 0 && (
+          <Text style={styles.textHargaTerbaru}>Harga terbaru</Text>
+        )}
+        <CardItemHargaUdang
+          data={item}
+          supplierName={
+            item.creator.email_verified ? item.creator.name : item.creator.buyer
+          }
+          verified={item.creator.email_verified ? true : false}
+          date={item.updated_at}
+          avatarId={item.creator.avatar}
+          regencyName={item.region.regency_name}
+          provinceName={item.region.province_name}
+          size={50}
+          onPressDetail={() => goToDetail(item)}
+        />
+      </View>
     );
   };
 
   return <FlatList data={data} renderItem={_renderItem} />;
 };
 export default TabHargaUdang;
+
+const styles = StyleSheet.create({
+  textHargaTerbaru: {
+    color: '#1971d4',
+    fontSize: 18,
+    textAlign: 'center',
+    marginTop: 10,
+    fontWeight: '600',
+    marginBottom: -8,
+  },
+});
